bf783d12ba1fd7187d6f07b1838236c353dfd1d1
baru-d <barudaniel6@gmail.com>
2023-09-26 09:15:16 +0300

    need more practice

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+let customerName = 'bob';
+
+function upperCaseCustomerName(){
+customerName = customerName.toUpperCase();
+}
+function setBestCustomer(){
+bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+var newBestCustomer = new Object;
+newBestCustomer.eVar1 = 'maybe bob';
+newBestCustomer.events = 'events2';
+}
+
+const LeastFavoriteCustomer = 'someone';
+
+function changeLeastFavoriteCustomer(){
+throw new Error('Assignment to constant variable.');
+}
